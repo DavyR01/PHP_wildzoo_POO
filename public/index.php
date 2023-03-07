f445f03bf0155ef8940496b29a559cb7a67ec4c5
davy <davy@bsglp1233.bluesoft-group.com>
2023-03-07 10:29:11 +0100

    quete finish before challenge

--- public/index.php
+++ public/index.php
@@ -5,21 +5,25 @@
 
 require __DIR__ . '/../src/Animal.php';
 
-$lion = new Animal();
-$lion->name = 'lion';
-$lion->pawNumber = 4;
+$lion = new Animal('lion', 4);
+// $lion->name = 'lion';
+// var_dump($lion);
+// $lion->pawNumber = 4;
 $lion->carnivorous = true;
-$lion->size = 70;
+// $lion->size = 70;
+// echo $lion->getSize();
+$lion->setSize(70);
 $lion->threatenedLevel = 'VU';
 
-$parrot = new Animal();
-$parrot->name = 'parrot';
-$parrot->pawNumber = 2;
-$parrot->size = 30;
+$parrot = new Animal('parrot', 2);
+// $parrot->name = 'parrot';
+// $parrot->pawNumber = 2;
+// $parrot->size = 30;
+$parrot->setSize(30);
 
-$elephant = new Animal();
-$elephant->name = 'elephant';
-$elephant->pawNumber = 4;
+$elephant = new Animal('elephant', 4);
+// $elephant->name = 'elephant';
+// $elephant->pawNumber = 4;
 $elephant->threatenedLevel = 'LC';
 
 $animals = [$lion, $parrot, $elephant];
@@ -30,4 +34,3 @@ $animals = [$lion, $parrot, $elephant];
 
 // Do not modify code below
 require 'view.php';
-?>
--- src/Animal.php
+++ src/Animal.php
@@ -3,7 +3,7 @@
 class Animal
 {
     public string $name;
-    public float $size = 100;
+    private float $size = 100;
     public bool $carnivorous = false;
     public int $pawNumber;
     public string $threatenedLevel = 'NE';
@@ -22,5 +22,34 @@ class Animal
     public function isDangerous(): bool
     {
         return $this->size > 50 && $this->carnivorous === true;
+    }
+
+    // Constructor
+    public function __construct(string $name, int $pawNumber)
+    {
+        $this->name = $name;
+        $this->setPawNumber($pawNumber);
+    }
+    public function getSize(): float
+    {
+        return $this->size;
+    }
+    public function setSize(int $size): void
+    {
+        if ($size <= 0) {
+            $size = 1;
+        }
+        $this->size = $size;
+    }
+    private function setPawNumber(int $pawNumber): void
+    {
+        if ($pawNumber < 0) {
+            $pawNumber = 0;
+        }
+        $this->pawNumber = $pawNumber;
+    }
+    public function getName(string $name)
+    {
+        return $this->name;
     }
 }
